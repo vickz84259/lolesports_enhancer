a4af3e981ba8b7c1f0b9f8a59469f6d095f6a1de
Victor Otieno Omondi <vickz84259@gmail.com>
2019-10-07 01:12:44 +0300

    Import spoiler file from previous repo
    The current state of the project is similar to what was in the previous
    repo. This includes the various bugs associated with hiding the
    spoilers.
    
    This commit acts as an "initial" project state.
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

---
+++ src/spoilers.js
@@ -0,0 +1,93 @@
+function hideSpoiler(match) {
+  let link = match.querySelector('a.past');
+
+  if (link !== null) {
+    let result = link.querySelector('.teams');
+
+    let classList = result.classList;
+    /* beautify ignore:start */
+    (classList.contains('winner-team2')) ?
+      classList.add('winner-team1') : classList.add('winner-team2');
+    /* beautify ignore:end */
+
+    let score = result.querySelector('.score');
+    if (score !== null) {
+      score.classList.replace('score', 'versus');
+    } else {
+      score = result.querySelector('.versus');
+    }
+    score.innerHTML = 'VS';
+
+    let teams = link.getElementsByClassName('team');
+    for (let team of teams) {
+      let matchHistory = team.querySelector('.team-info .winloss');
+      if (matchHistory !== null) {
+        matchHistory.remove();
+      }
+
+      let teamName = team.querySelector('.team-info h2');
+      teamName.setAttribute('style', 'color: #555d64');
+    }
+  }
+}
+
+
+function observeMatches() {
+  let observer = new MutationObserver(mutationRecords => {
+    for (let mutationRecord of mutationRecords) {
+      let event = mutationRecord.target;
+      for (let eventMatch of event.getElementsByClassName('EventMatch')) {
+        hideSpoiler(eventMatch);
+      }
+    }
+  });
+
+  let config = {
+    childList: true,
+  };
+  let targetElement = document.body.querySelector('.Schedule .events .Event');
+  observer.observe(targetElement, config);
+}
+
+function* filteredNodes(nodes) {
+  for (let node of nodes) {
+    if (node.nodeType === 1) {
+      yield node;
+    }
+  }
+}
+
+function* recordsIterator(mutationRecords) {
+  for (let mutationRecord of mutationRecords) {
+    yield* filteredNodes(mutationRecord.addedNodes);
+  }
+}
+
+function processEventNode(node) {
+  for (let match of filteredNodes(node.childNodes)) {
+    if (match.classList.contains('EventMatch')) {
+      hideSpoiler(match);
+    }
+  }
+}
+
+function init() {
+  let observer = new MutationObserver((mutationRecords, observer) => {
+    for (let node of recordsIterator(mutationRecords)) {
+      if (node.classList.contains('Event')) {
+        processEventNode(node);
+
+        observer.disconnect();
+        observeMatches();
+        break;
+      }
+    }
+  });
+  let config = {
+    childList: true,
+    subtree: true,
+  };
+  observer.observe(document.body, config);
+}
+
+init();
