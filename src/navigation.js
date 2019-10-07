cd23120e43e78953f9ee27f7d17d8b201e073719
Victor Otieno Omondi <vickz84259@gmail.com>
2019-10-07 20:42:44 +0300

    Implement background navigation tracker
    This keeps track of the various lolesports tabs and the urls navigated
    to. This information is sent to the content scripts so that they can
    analyse whether to connect or disconnect any mutation observers.
    
    Also, due to the way lolesports changes links, content scripts are now
    added to the whole website rather than select paths.
    
    The changes to the manifest also deal with the situations, where there's a
    subdomain like: watch.euw.lolesports.com. Although, at the time of
    writing, the website doesn't use this format. It has been used before though.
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

---
+++ src/navigation.js
@@ -0,0 +1,37 @@
+const PORTS = new Map();
+
+function main() {
+  browser.runtime.onConnect.addListener(port => {
+    tabId = port.sender.tab.id;
+    PORTS.set(tabId, port);
+  });
+
+  browser.tabs.onRemoved.addListener(tabId => PORTS.delete(tabId));
+
+  /* beautify preserve:start */
+  browser.webNavigation.onHistoryStateUpdated.addListener(
+    navigationListener,
+    {
+      url: [{
+        schemes: ['https'],
+        hostSuffix: 'watch.lolesports.com'
+      },
+      {
+        schemes: ['https'],
+        hostContains: '.lolesports.com'
+      }]
+    }
+  );
+  /* beautify preserve:end */
+}
+
+function navigationListener(details) {
+  let port = PORTS.get(details.tabId);
+
+  let message = {
+    url: details.url
+  };
+  port.postMessage(message);
+}
+
+main();
