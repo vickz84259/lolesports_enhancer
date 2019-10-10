ebb27755545d59331a08f77dcc56f952fb8e2e11
Victor Otieno Omondi <vickz84259@gmail.com>
2019-10-10 19:45:12 +0300

    Implement tracker object
    This object is used to keep track of which page the user is currently on.
    This information is used by the content script to know when to connect
    or disconnect the spoilers MutationObservers.
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- src/spoilers.js
+++ src/spoilers.js
@@ -1,5 +1,43 @@
 'use strict';
 
+class Info {
+
+  constructor() {
+    // The tab the content script is currently running in
+    this.tabId = null;
+
+    // Keep track of the page the tab user visited and is currently on
+    this._previousLink = null;
+    this._currentLink = null;
+
+    this._regexPattern = /^https:\/\/watch\.(?:\w+\.)?lolesports\.com\/schedule(?:\?\S+)?$/;
+  }
+
+  handleMessage(message) {
+    if (!this.tabId) this.tabId = message.tabId.toString();
+
+    if (this._currentLink) this._previousLink = this._currentLink;
+    this._currentLink = message.url;
+
+    let matchesPrevious = this._regexPattern.test(this._previousLink);
+    let matchesCurrent = this._regexPattern.test(this._currentLink);
+
+    /* beautify preserve:start */
+    let value = {};
+    if (!matchesPrevious && matchesCurrent) {
+      // This means the user has navigated to the page of interest
+      value = { status: 'initial' };
+    } else if (matchesPrevious && !matchesCurrent) {
+      // This means the user has navigated away from the page of interest
+      value = { status: 'disconnect' };
+    }
+    if (!isEmpty(value)) setToStorage(this.tabId, value);
+    /* beautify preserve:end */
+  }
+}
+
+const INFO = new Info();
+
 function hideSpoiler(match) {
   let link = match.querySelector('a.past');
 
@@ -90,6 +128,11 @@ function init() {
     subtree: true,
   };
   observer.observe(document.body, config);
+
+  let port = browser.runtime.connect();
+  port.onMessage.addListener(message => INFO.handleMessage(message));
+
+  browser.storage.onChanged.addListener(changes => console.log(changes));
 }
 
 function isEmpty(object) {
