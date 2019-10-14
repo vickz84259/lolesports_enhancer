5d5fae815ca71c45e7fff8b63a0e072a436ad69a
Victor Otieno Omondi <vickz84259@gmail.com>
2019-10-15 01:23:39 +0300

    Implement basic vod content script
    This script will handle the necessary DOM manipulations when the user is
    viewing a VOD
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
-export default {
-  input: 'src/js/content_scripts/spoilers.js',
-  output: {
-    file: 'builds/spoilers.js',
-    format: 'iife'
+export default [{
+    input: 'src/js/content_scripts/spoilers.js',
+    output: {
+      file: 'builds/spoilers.js',
+      format: 'iife'
+    }
+  },
+  {
+    input: 'src/js/content_scripts/vod.js',
+    output: {
+      file: 'builds/vod.js',
+      format: 'iife'
+    }
   }
-}
+]
---
+++ src/js/content_scripts/vod.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import * as link_state from '../modules/link_state.js';
+import { getElementBySelector } from '../modules/DOM_utils.js';
+
+/* Currently the function works as intended but there are improvements that needed.
+TODO:
+  Find how to restore cookie button to previous state
+  Fix issue where "Login for watch rewards" popup removes the button in its new
+    position*/
+async function moveCookieButton() {
+  let cookieButton = getElementBySelector('.riotbar-cookie-policy-v2.cookie-link');
+  let streamSelector = getElementBySelector('.stream-selector');
+
+  let nav = await getElementBySelector('.nav-details .nav');
+  nav.insertBefore((await cookieButton), (await streamSelector));
+}
+
+function statusHandler(tabState) {
+  if (tabState.action === 'initialise') {
+    // moveCookieButton();
+  }
+}
+
+let link_regex = /https:\/\/watch\.(?:\w+\.)?lolesports\.com\/vod\/\d+\/\d{1}(?:\/[a-zA-Z0-9_\-]{11})?/;
+let properties = {
+  portName: 'vod',
+  regexPattern: link_regex,
+  handler: statusHandler
+};
+link_state.connect(properties);
