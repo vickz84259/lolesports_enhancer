3208a222489bc0e36a5536c9cf6c260df0418dad
Victor Otieno Omondi <vickz84259@gmail.com>
2019-10-22 04:00:15 +0300

    Refactor sidebar.js
    Since the click listeners for the left and right sidebar options were
    very similar, it made sense to be able to simplify them into a single
    function.
    
    Using closures the above goal is achieved. Also the closure is used
    to store the state of the size of the left sidebar. This is in anticipation
    of various screen resolutions having separate sizes.
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- src/js/modules/layout/sidebar.js
+++ src/js/modules/layout/sidebar.js
@@ -22,56 +22,50 @@ async function getSelectedOption() {
   }
 }
 
-async function rightSBLayoutHandler(event) {
-  // function that gets called when the right sidebar layout option is clicked
-  let option = await getSelectedOption();
-  if (option.firstChild.textContent === Layouts.SIDEBAR_LEFT) {
-    // move the side bar to the right
-    let mainArea = await getElementBySelector('.Watch.large');
-    mainArea.setAttribute('style', 'flex-direction: row-reverse;');
+async function getSBLayoutHandler(layout) {
+  let videoPlayer = await getElementById('video-player');
+  const leftStyle = videoPlayer.style.left;
 
-    // move the video player to the left
-    let videoPlayer = await getElementById('video-player');
-    videoPlayer.style.left = '0px';
+  const isLeftSB = layout === Layouts.SIDEBAR_LEFT;
 
-    // change the layout options UI
-    option.classList.remove('selected');
-    event.currentTarget.classList.add('selected');
-    setCurrentLayout(Layouts.SIDEBAR_RIGHT);
+  return async function(event) {
+    // function that gets called when a sidebar layout option is clicked
+    let option = await getSelectedOption();
+    let oppositeLayout = (isLeftSB) ? Layouts.SIDEBAR_RIGHT : Layouts.SIDEBAR_LEFT;
+    if (option.firstChild.textContent === oppositeLayout) {
+
+      // move the side bar left or right
+      let mainArea = await getElementBySelector('.Watch.large');
+      let flex = (isLeftSB) ? 'flex-direction: row;' : 'flex-direction: row-reverse;';
+      mainArea.setAttribute('style', flex);
+
+      // move the video player to the left or right
+      videoPlayer = await getElementById('video-player');
+      videoPlayer.style.left = (isLeftSB) ? leftStyle : '0px';
+      videoPlayer = null;
+
+      // change the layout options UI
+      option.classList.remove('selected');
+      event.currentTarget.classList.add('selected');
+      setCurrentLayout(layout);
+    }
   }
+
 }
 
-async function leftSBLayouthandler(event) {
-  // function that gets called when the left sidebar layout option is clicked
-  let option = await getSelectedOption();
-  if (option.firstChild.textContent === Layouts.SIDEBAR_RIGHT) {
-    // move the side bar to the left
-    let mainArea = await getElementBySelector('.Watch.large');
-    mainArea.setAttribute('style', 'flex-direction: row');
-
-    // move the video player to the right
-    let videoPlayer = await getElementById('video-player');
-    videoPlayer.style.left = '400px';
-
-    // change the layout options UI
-    option.classList.remove('selected');
-    event.currentTarget.classList.add('selected');
-    setCurrentLayout(Layouts.SIDEBAR_LEFT);
-  }
-}
-
-async function setLayoutOptions() {
+async function setExtraOption() {
   for await (let element of getOptions()) {
     let firstChild = element.firstChild;
     if (firstChild.textContent === 'Sidebar') {
       // Renaming the previous option as Sidebar Left
       firstChild.textContent = Layouts.SIDEBAR_LEFT;
 
-      element.addEventListener('click', leftSBLayouthandler);
+      let handler = await getSBLayoutHandler(Layouts.SIDEBAR_LEFT);
+      element.addEventListener('click', handler);
     }
   }
 
-  // Creating the extra layout option
+  // Creating the extra layout option (Sidebar Right)
   let newOption = document.createElement('li');
   newOption.setAttribute('class', 'option');
 
@@ -87,16 +81,8 @@ async function setLayoutOptions() {
   let sidebarSVG = await (await fetch(url)).text();
 
   newOption.insertAdjacentHTML('beforeend', DOMPurify.sanitize(sidebarSVG));
-  newOption.addEventListener('click', rightSBLayoutHandler);
-}
-
-async function setSelectedLayout() {
-  let currentLayout = await getCurrentLayout();
-  for await (let option of getOptions()) {
-    if (option.firstChild.textContent === currentLayout) {
-      option.classList.add('selected');
-    }
-  }
+  let handler = await getSBLayoutHandler(Layouts.SIDEBAR_RIGHT)
+  newOption.addEventListener('click', handler);
 }
 
 async function setUpLayoutObserver(tabState) {
@@ -105,8 +91,15 @@ async function setUpLayoutObserver(tabState) {
   tabState.observer = new MutationObserver(async (mutationRecords) => {
     for (let element of mutation.recordsIterator(mutationRecords)) {
       if (element.className === 'WatchOptionsLayout') {
-        await setLayoutOptions();
-        setSelectedLayout();
+        await setExtraOption();
+
+        // Set UI for currently selected layout
+        let currentLayout = await getCurrentLayout();
+        for await (let option of getOptions()) {
+          if (option.firstChild.textContent === currentLayout) {
+            option.classList.add('selected');
+          }
+        }
       }
     }
   });
