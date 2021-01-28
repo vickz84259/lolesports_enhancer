487efaa897cc00da249b2dc95da4690aee7cd7ea
Victor Otieno <vickz84259@gmail.com>
2021-01-29 01:29:50 +0300

    Migrate backround_scripts/navigation.js to Typescript
    Signed-off-by: Victor Otieno <vickz84259@gmail.com>

--- src/js/background_scripts/navigation.ts
+++ src/js/background_scripts/navigation.ts
@@ -1,17 +1,15 @@
-/** @type {Map<string, browser.runtime.Port>} */
-const PORTS = new Map();
+const PORTS: Map<string, browser.runtime.Port> = new Map();
 
 // Keeps track of the unique ids representing ports for each content script in
 // a particular tab
-/** @type {Map<number, string[]>} */
-const IDS_MAP = new Map();
+const IDS_MAP: Map<number, string[]> = new Map();
 
 function main() {
   browser.runtime.onConnect.addListener(port => {
-    const tabId = port.sender.tab.id;
+    const tabId = port.sender!.tab!.id!;
 
     const unique_id = `${tabId}_${port.name}`;
-    const value = IDS_MAP.has(tabId) ? IDS_MAP.get(tabId) : [];
+    const value = IDS_MAP.has(tabId) ? IDS_MAP.get(tabId)! : [];
     value.push(unique_id);
 
     IDS_MAP.set(tabId, value);
@@ -20,7 +18,7 @@ function main() {
 
   browser.tabs.onRemoved.addListener(tabId => {
     if (IDS_MAP.has(tabId)) {
-      const ids = IDS_MAP.get(tabId);
+      const ids = IDS_MAP.get(tabId)!;
       for (const id of ids) {
         PORTS.delete(id);
       }
@@ -47,32 +45,33 @@ function main() {
   );
 }
 
-/**
- * Contains details about the navigation event.
- * @typedef {Object} NavigationDetails
- * @property {number} tabId - the ID of the tab in which the navigation is about
- *    to occur.
- * @property {string} url - The URL to which the given frame will navigate.
- * @property {number} processId
- * @property {number} frameId
- * @property {number} timeStamp
- * @property {browser.webNavigation.TransitionType} transitionType
- * @property {browser.webNavigation.TransitionQualifier[]} transitionQualifiers
- */
+
+// Contains details about the navigation event.
+interface NavigationDetails {
+  tabId: number, // The ID of the tab in which the navigation is about to occur.
+  url: string, // The URL to which the given frame will navigate.
+  processId: number,
+  frameId: number,
+  timeStamp: number,
+  transitionType: browser.webNavigation.TransitionType,
+  transitionQualifiers: browser.webNavigation.TransitionQualifier[]
+}
 
 /**
  * Callback function to handle onHistoryStateUpdated events
  * The function sends a message to every content script on the particular tab
  * where the link has changed.
  *
- * @param {NavigationDetails} details
+ * @param details
  */
-function navigationListener(details) {
-  const ids = IDS_MAP.get(details.tabId);
+function navigationListener(details: NavigationDetails) {
+  const ids = IDS_MAP.get(details.tabId)!;
   for (const id of ids) {
-    const port = PORTS.get(id);
+    const port = PORTS.get(id)!;
     port.postMessage({ url: details.url });
   }
 }
 
 main();
+
+export {};
